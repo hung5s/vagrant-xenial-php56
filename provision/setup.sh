5d9e56456f89780305766c56cd855af232376777
hung5s <hung5s@gmail.com>
2017-10-07 15:34:30 +0700

    Initial commit

---
+++ provision/setup.sh
@@ -0,0 +1,24 @@
+apt-get update
+
+## Section: Nginx
+apt-get install -y nginx
+
+sudo cp /tmp/default /etc/nginx/sites-available/
+sudo cp /tmp/indition.conf /etc/nginx/snippets/
+
+sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/ssl-cert-snakeoil.key -out /etc/ssl/certs/ssl-cert-snakeoil.pem
+
+sudo systemctl restart nginx
+
+## Section: PHP
+
+# remove old PHP
+sudo apt-get purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "` 
+
+# Add PPA source
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update 
+
+# Install
+sudo apt-get install -y php5.6 php5.6-cli php5.6-fpm
+sudo apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-pgsql php5.6-curl php5.6-soap
