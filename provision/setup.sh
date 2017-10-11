850df2ea973ac85efc84813e909b70eac7a00504
hung5s <hung5s@gmail.com>
2017-10-11 22:11:57 +0700

    support PHP for localhost site, install php-zip

--- provision/setup.sh
+++ provision/setup.sh
@@ -21,4 +21,4 @@ sudo apt-get update
 
 # Install
 sudo apt-get install -y php5.6 php5.6-cli php5.6-fpm
-sudo apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-pgsql php5.6-curl php5.6-soap
+sudo apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-pgsql php5.6-curl php5.6-soap php5.6-zip
